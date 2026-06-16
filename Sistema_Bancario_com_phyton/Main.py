3956955e37dfe7595eb22cde067b02488b9b5e1b
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2026-06-15 21:19:50 -0300

    Sistema Bancário com Python
    Sistema bancario com python.

---
+++ Sistema_Bancario_com_phyton/Main.py
@@ -0,0 +1,6 @@
+from Jefferson_Simplicio_Portfólio_Dev.Sistema_Bancario_com_phyton.library.interface import *
+from Jefferson_Simplicio_Portfólio_Dev.Sistema_Bancario_com_phyton.library.Program import  *
+
+while True:
+    selecao = menu(['Depositar', 'Sacar', 'Extrato', 'Sair' ])
+    opcao (selecao)
